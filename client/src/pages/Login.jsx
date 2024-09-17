9342448e4a268043f0a14ae9856c29de923ac109
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-09-18 00:31:59 +0500

    Make The App Routes For Navigation

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <h1>Login</h1>
+    </main>
   )
 }
 
