3983b77fc001934ddb9cfe4dd48b23241f59e4a4
Hamza Ilyas <Hamzafullstackdev1@gmail.com>
2024-09-19 00:22:23 +0500

    Design The Navbar For The Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Users from "./pages/Users";
 import Trash from "./pages/Trash";
 import { useSelector } from "react-redux";
 import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
 
 function Layout() {
   const { user } = useSelector((state)=>state.auth);
@@ -24,7 +25,7 @@ function Layout() {
       {/* <MobileSidebar /> */}
 
       <div className='flex-1 overflow-y-auto'>
-        {/* <Navbar /> */}
+        <Navbar />
 
         <div className='p-4 2xl:px-10'>
           <Outlet />
